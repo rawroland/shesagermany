40c561b3410a310d67f3b85bb16f18427eb3d941
Roland Awemo <rawroro@googlemail.com>
2014-12-13 01:07:57 +0100

    First commit

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity.
+ */
+class User extends Entity {
+
+/**
+ * Fields that can be mass assigned using newEntity() or patchEntity().
+ *
+ * @var array
+ */
+	protected $_accessible = [
+		'email' => true,
+		'password' => true,
+		'title' => true,
+		'name' => true,
+		'surname' => true,
+	];
+
+}
---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ */
+class UsersTable extends Table {
+
+/**
+ * Initialize method
+ *
+ * @param array $config The configuration for the Table.
+ * @return void
+ */
+	public function initialize(array $config) {
+		$this->table('users');
+		$this->displayField('name');
+		$this->primaryKey('id');
+		$this->addBehavior('Timestamp');
+	}
+
+/**
+ * Default validation rules.
+ *
+ * @param \Cake\Validation\Validator $validator instance
+ * @return \Cake\Validation\Validator
+ */
+	public function validationDefault(Validator $validator) {
+		$validator
+			->add('id', 'valid', ['rule' => 'numeric'])
+			->allowEmpty('id', 'create')
+			->add('email', 'valid', ['rule' => 'email'])
+			->requirePresence('email', 'create')
+			->notEmpty('email')
+			->add('email', 'unique', ['rule' => 'validateUnique', 'provider' => 'table'])
+			->requirePresence('password', 'create')
+			->notEmpty('password')
+			->allowEmpty('title')
+			->requirePresence('name', 'create')
+			->notEmpty('name')
+			->requirePresence('surname', 'create')
+			->notEmpty('surname');
+
+		return $validator;
+	}
+
+}
