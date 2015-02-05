9a91250af4c7faefe0cb03c402c5ae7c597cbe43
Roland Awemo <rawroro@googlemail.com>
2015-02-05 22:19:30 +0100

    Add Events with integration test

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -1,17 +1,26 @@
 <?php
 namespace App\Controller;
 
+
 /**
  * Class EventsController
  * @package App\Controller
  * @property \App\Model\Table\EventsTable $Events
+ * @property \Cake\Controller\Component\FlashComponent $Flash
  */
 class EventsController extends AppController{
 
 
   public function add() {
     $event = $this->Events->newEntity($this->request->data());
-
+    if($this->request->is('post')) {
+      if($this->Events->save($event)) {
+        $this->Flash->success('The Event was successfully saved.');
+        return $this->redirect(['action' => 'index']);
+      } else {
+        $this->Flash->error('The Event could not be saved. Please check the submitted data and try again.');
+      }
+    }
     $this->set(compact('event'));
   }
 } 
--- tests/TestCase/Controller/EventsControllerTest.php
+++ tests/TestCase/Controller/EventsControllerTest.php
@@ -18,12 +18,22 @@ class EventsControllerTest extends IntegrationTestCase {
 		'events' => 'app.events'
 	];
 
+  public $data = [
+    'title' => 'SHESA Germany SG7',
+    'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et.',
+    'date' => '2015-07-11',
+  ];
+
+  public $Events = null;
+
   public function setUp() {
     parent::setUp();
+    $this->Events = TableRegistry::get('Events');
   }
 
   public function tearDown() {
     parent::tearDown();
+    $this->Events = null;
   }
 
   public function testAddPageLoads() {
@@ -31,11 +41,24 @@ class EventsControllerTest extends IntegrationTestCase {
     $this->assertResponseOk();
   }
 
-  public function testAddFailsWithInvalidData() {
+  public function testAddSuccessfully() {
+    $this->post('/events/add', $this->data);
+    $expected = __('The Event was successfully saved.');
+    $this->assertSession($expected, 'Flash.flash.message', 'Correct flash message was not displayed.');
+    $query = $this->Events->find('all');
+    $expected = 11;
+    $actual = $query->count();
+    $this->assertEquals($expected, $actual, 'Number of events did not increase in the database.');
+  }
+
+  public function testAddFailsWithEmptyData() {
     $data = [];
     $this->post('/events/add', $data);
-    $events = TableRegistry::get('Events');
-    $query = $events->find('all');
-    $this->assertEquals(10, $query->count(), 'No Events were added without a title');
+    $expected = __('The Event could not be saved. Please check the submitted data and try again.');
+    $this->assertSession($expected, 'Flash.flash.message', 'Correct flash message was not displayed.');
+    $query = $this->Events->find('all');
+    $expected = 10;
+    $actual = $query->count();
+    $this->assertEquals($expected, $actual, 'No Events were added without a title');
   }
 }
