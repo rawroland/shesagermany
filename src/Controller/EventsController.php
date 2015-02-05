f160d71ea308367456742a29f4af8ff09adbeb07
Roland Awemo <rawroro@googlemail.com>
2015-02-05 22:40:05 +0100

    Decreasing messiness

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -10,16 +10,17 @@ namespace App\Controller;
  */
 class EventsController extends AppController{
 
-
+  /**
+   * Action for adding events.
+   */
   public function add() {
     $event = $this->Events->newEntity($this->request->data());
     if($this->request->is('post')) {
       if($this->Events->save($event)) {
         $this->Flash->success('The Event was successfully saved.');
         return $this->redirect(['action' => 'index']);
-      } else {
-        $this->Flash->error('The Event could not be saved. Please check the submitted data and try again.');
       }
+      $this->Flash->error('The Event could not be saved. Please check the submitted data and try again.');
     }
     $this->set(compact('event'));
   }
