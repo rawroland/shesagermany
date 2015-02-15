e39d3675462b2317c8d3227097eb5227c54acee5
Roland Awemo <rawroro@googlemail.com>
2015-02-15 16:51:04 +0100

    Fixed Coding standard

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -19,17 +19,18 @@ use Cake\View\View;
  * App View class
  *
  */
-class AppView extends View {
+class AppView extends View
+{
 
-/**
- * Initialization hook method.
- *
- * For e.g. use this method to load a helper for all views:
- * `$this->loadHelper('Html');`
- *
- * @return void
- */
-	public function initialize() {
-	}
-
+    /**
+     * Initialization hook method.
+     *
+     * For e.g. use this method to load a helper for all views:
+     * `$this->loadHelper('Html');`
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+    }
 }
