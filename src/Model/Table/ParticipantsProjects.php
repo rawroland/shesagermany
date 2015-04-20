605a0e6c39b2d7b9ea7bfb4937a92b9b8d27f93e
Roland Awemo <rawroro@googlemail.com>
2015-04-21 00:43:56 +0200

    Projects view

---
+++ src/Model/Table/ParticipantsProjects.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Roland Awemo
+ * Date: 18.04.15
+ * Time: 17:13
+ */
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class ParticipantsProjects extends Table
+{
+
+    public function initialize(array $config) {
+        parent::initialize($config);
+        $this->belongsTo('Activities');
+        $this->belongsTo('Projects');
+    }
+} 
